253a601f31063124fe2c477c6b6898a0e2ab64ed
seri <seri114@gmail.com>
2023-04-19 23:18:38 +0900

    .

--- src/logger.py
+++ src/logger.py
@@ -2,42 +2,6 @@ import os
 import logging
 import logging.handlers
 
-
-class CustomFormatter(logging.Formatter):
-    __LEVEL_COLORS = [
-        (logging.DEBUG, '\x1b[40;1m'),
-        (logging.INFO, '\x1b[34;1m'),
-        (logging.WARNING, '\x1b[33;1m'),
-        (logging.ERROR, '\x1b[31m'),
-        (logging.CRITICAL, '\x1b[41m'),
-    ]
-    __FORMATS = None
-
-    @classmethod
-    def get_formats(cls):
-        if cls.__FORMATS is None:
-            cls.__FORMATS = {
-                level: logging.Formatter(
-                    f'\x1b[30;1m%(asctime)s\x1b[0m {color}%(levelname)-8s\x1b[0m \x1b[35m%(name)s\x1b[0m -> %(message)s',
-                    '%Y-%m-%d %H:%M:%S'
-                )
-                for level, color in cls.__LEVEL_COLORS
-            }
-        return cls.__FORMATS
-
-    def format(self, record):
-        formatter = self.get_formats().get(record.levelno)
-        if formatter is None:
-            formatter = self.get_formats()[logging.DEBUG]
-        if record.exc_info:
-            text = formatter.formatException(record.exc_info)
-            record.exc_text = f'\x1b[31m{text}\x1b[0m'
-
-        output = formatter.format(record)
-        record.exc_text = None
-        return output
-
-
 class LoggerFactory:
     @staticmethod
     def create_logger(formatter, handlers):
@@ -49,18 +13,13 @@ class LoggerFactory:
             logger.addHandler(handler)
         return logger
 
-
-class FileHandler(logging.FileHandler):
-    def __init__(self, log_file):
-        os.makedirs(os.path.dirname(log_file), exist_ok=True)
-        super().__init__(log_file)
-
+rotate_handler = logging.handlers.RotatingFileHandler("output.log", maxBytes=1000000, backupCount=5)
 
 class ConsoleHandler(logging.StreamHandler):
     pass
 
 
-formatter = CustomFormatter()
-file_handler = FileHandler('./logs')
+formatter = "%(asctime)s [%(levelname)s] %(message)s"
+# file_handler = FileHandler('./logs')
 console_handler = ConsoleHandler()
-logger = LoggerFactory.create_logger(formatter, [file_handler, console_handler])
+logger = LoggerFactory.create_logger(logging.Formatter(formatter), [rotate_handler, console_handler])
